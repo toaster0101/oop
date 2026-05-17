8c827ad15b49c1946cdd1182f403f79121fd98c7
toaster0101 <henry.bray.star@gmail.com>
2026-05-17 15:59:30 +0100

    Add files via upload

--- flappy bird/flappy bird.py
+++ flappy bird/flappy bird.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 pygame.init()
 WIDTH=800
 HEIGHT=600
@@ -7,9 +8,14 @@ screen.fill("black")
 groundX=0
 TPS=60
 flying=False
+score=0
+gameover=False
+pipeFrec=2000
+lastPipe=pygame.time.get_ticks()-2000
 
 background=pygame.image.load("flappy bird/images/bg.png")
 ground=pygame.image.load("flappy bird/images/ground.png")
+restart=pygame.image.load("flappy bird/images/restart.png")
 
 class birdAnim(pygame.sprite.Sprite):
     def __init__(self,x,y):
@@ -26,17 +32,19 @@ class birdAnim(pygame.sprite.Sprite):
         self.velocity=0
         self.clicked=False
     def update(self):
-        global flying
-        self.counter+=1
-        if self.counter==5:
-            self.index+=1
-            if self.index==3:
-                self.index=0
-            self.image=self.images[self.index]
-            self.counter=0
+        global flying, gameover
+        if not gameover:
+            self.counter+=1
+            if self.counter==5:
+                self.index+=1
+                if self.index==3:
+                    self.index=0
+                self.image=self.images[self.index]
+                self.counter=0
         if flying:
             self.velocity+=0.2
             self.rect.y+=self.velocity
+        if flying and not gameover:
             if pygame.mouse.get_pressed()[0]==1 and self.clicked==False:
                 self.velocity=0
                 self.velocity-=7
@@ -45,25 +53,72 @@ class birdAnim(pygame.sprite.Sprite):
                 self.clicked=False
             if self.rect.y>460:
                 flying=False
+                gameover=True
+                self.rect.y=470
+        self.image=pygame.transform.rotate(self.images[self.index],self.velocity*-2)
+
+class pipes(pygame.sprite.Sprite):
+    def __init__(self,x,y,pipePos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image=pygame.image.load("flappy bird/images/pipe.png")
+        self.rect=self.image.get_rect()
+        if pipePos==-1:
+            self.rect.topleft=[x,y]
+        if pipePos==1:
+            self.image=pygame.transform.flip(self.image,False,True)
+            self.rect.bottomleft=[x,y]
+        self.passed=False
+    def update(self):
+        if not gameover:
+            self.rect.x-=2
+        if self.rect.right<0:
+            self.kill()
+
 
 bird_group=pygame.sprite.Group()
-mainBird=birdAnim(75,300)
+mainBird=birdAnim(150,300)
 bird_group.add(mainBird)
 
+pipe_group=pygame.sprite.Group()
+
 clock=pygame.time.Clock()
+
+font1=pygame.font.SysFont("impact",25)
 
 while True:
     clock.tick(TPS)
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
-        if event.type==pygame.MOUSEBUTTONDOWN:
+        if event.type==pygame.MOUSEBUTTONDOWN and not gameover:
             flying=True
     screen.blit(background,(0,-100))
+    currentTime=pygame.time.get_ticks()
+    if (currentTime-lastPipe)>pipeFrec and flying and not gameover:
+        pipeYDiff=random.randint(-100,100)
+        mainPipe=pipes(800,400+pipeYDiff,-1)
+        topPipe=pipes(800,200+pipeYDiff,1)
+        pipe_group.add(mainPipe)
+        pipe_group.add(topPipe)
+        lastPipe=currentTime
+    pipe_group.draw(screen)
+    pipe_group.update()
     screen.blit(ground,(groundX,500))
-    groundX-=1
+    if not gameover:
+        groundX-=2
     if groundX<-35:
         groundX=0
+    text1=font1.render("Score = "+str(int(score)),True,"black")
+    screen.blit(text1,(10,10))
+    if pygame.sprite.groupcollide(bird_group,pipe_group,False,False):
+        gameover=True
+    for i in pipe_group:
+        if i.rect.right<bird_group.sprites()[0].rect.left and not i.passed:
+            i.passed=True
+            score+=0.5
+    if gameover:
+        restartHit=restart.get_rect(center=(400,300))
+        screen.blit(restart,(restartHit))
     bird_group.draw(screen)
     bird_group.update()
     pygame.display.update()
